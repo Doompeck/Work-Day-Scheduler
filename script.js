dd186dde8597f85f9c0e4ec8a0d70297ab1d5d00
Benjamin <bstanf@uw.edu>
2022-11-29 19:56:17 -0800

    roughed in my for loop and html template-literal

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 var containerEl = $(".container");
 var currentDayEl = $("#currentDay");
 // Need to be able to declare the current date and time in total
-currentDayEl.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+currentDayEl.text(moment().format("MMMM Do YYYY"));
 // will need to use moment to determine the current hour
-// also use moment to declare the current day
+var currentHour = moment().format("HH");
 
 // will need an array to map available hours
 var dayTimeArray = [
@@ -25,7 +25,21 @@ var dayTimeArray = [
 // Will need an if statement in here that determines past, present, or future
 function timeBlock() {
 
+    for( i=0; i < dayTimeArray.length; i++) {
+        
+        htmlTemplate = `
+        <div class="row">
+        <div class="hour"></div>
+        <textarea class=""></textarea>
+        <button class="saveBtn">Save</button>
+        </div>
+        `
+
+    };
+
 };
 // Will need an event listener for the container element
 // This will be used to save the data for the clicked time's data into local storage.
-
+containerEl.addEventListener("click", function(event) {
+    event.preventDefault();
+});
