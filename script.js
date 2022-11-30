e387b44377defc83c11b7108b0a553a430d8f1e1
Benjamin <bstanf@uw.edu>
2022-11-29 18:02:20 -0800

    Initial commit with repo structure and pseudo-code

---
+++ script.js
@@ -0,0 +1,17 @@
+// Need to be able to declare the current time,
+// declare needed html elements that will be needed:
+// (the currentDay id, the container block that the timeblocks will load into, the jumbotron)
+
+// will need to use moment to determine the current hour
+
+// also use moment to declare the current day
+
+// will need an array to map available hours
+
+// For loop to render a block element for each hour of the day.
+// This will include our template html that describes said block
+// Will need an if statement in here that determines past, present, or future
+
+// Will need an event listener for the container element
+// This will be used to save the data for the clicked time's data into local storage.
+
